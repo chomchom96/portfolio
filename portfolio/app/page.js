06db683cf6c082cad0448fb1e3392ba37fc808cb
chomchom96 <cyy8071@gmail.com>
2024-07-16 12:51:57 +0900

    avatar

--- portfolio/app/page.js
+++ portfolio/app/page.js
@@ -24,22 +24,26 @@ export default function Home() {
           <div className="my-10">
             {/* 사진? */}
             <div className="grid grid-rows-2 grid-cols-2 w-full h-[30vh]">
+              {/* 이모티콘 + 설명 */}
               <div className="size-24 "></div>
               <div>메일</div>
               <div>깃허브</div>
               <div>블로그</div>
             </div>
-            <div>
-              <h1>
-                저는 수학 문제 푸는 것을 좋아해서 수학과 전공을 선택하였습니다.{" "}
-                <br />
+            <div className="border-4 border-blue-500 rounded-xl p-5 shadow-lg space-y-2">
+              <p>
+                저는 수학 문제 푸는 것을 좋아해서 수학과 전공을 선택하였습니다.
+              </p>
+              <p>
                 마크업 언어에 대한 호기심에 혼자서 관련 영상과 서적을 찾아
                 이것저것 시도해보면서 자연스럽게 프론트엔드 개발자를 꿈꾸게
                 되었습니다.
-                <br /> 수많은 도전과 경험을 통해 다양한 기술 스택을 쌓아
-                적재적소에 알맞은 기술을 적용하여 최고의 UX를 제공할 수 있는
-                프론트엔드 개발자가 되고 싶습니다.{" "}
-              </h1>
+              </p>
+              <p>
+                수많은 도전과 경험을 통해 다양한 기술 스택을 쌓아 적재적소에
+                알맞은 기술을 적용하여 최고의 UX를 제공할 수 있는 프론트엔드
+                개발자가 되고 싶습니다.
+              </p>
             </div>
           </div>
         </div>
@@ -53,7 +57,7 @@ export default function Home() {
             ls
           </h1>
           {/* TODO : 기술아이콘 + 옆에 할 수 있는 거 */}
-          <div className="flex flex-row justify-around gap-4 border-4 border-cyan-500 rounded-lg my-10 p-5">
+          <div className="flex flex-row justify-around gap-4 border-4 border-cyan-500 rounded-xl my-10 p-5">
             <div className="flex flex-col shadow-sm rounded-lg">
               <h1>Frontend</h1>
               <div className="flex flex-row">
