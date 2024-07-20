cbe05d252b99e93d6f9dcddfcf60ff191b4ba887
chomchom96 <cyy8071@gmail.com>
2024-07-20 23:36:54 +0900

    vercel features

--- portfolio/app/layout.js
+++ portfolio/app/layout.js
@@ -1,4 +1,6 @@
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 
 export const metadata = {
   title: "조용운의 포트폴리오입니다.",
@@ -9,6 +11,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="ko">
       <body>{children}</body>
+      <Analytics />
+      <SpeedInsights />
     </html>
   );
 }
